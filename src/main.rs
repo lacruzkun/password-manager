edaaf26f301a2e1317323ea490992e31b680ffd9
lacruzkun <crabraham0@gmail.com>
2025-12-06 20:33:54 +0100

    handled all errors

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
-use libc::{tcgetattr, tcsetattr, termios, ECHO, ICANON, TCSANOW};
+use libc::{ECHO, ICANON, TCSANOW, tcgetattr, tcsetattr, termios};
+use std::error::Error;
 use std::fs::{File, OpenOptions};
 use std::hash::{DefaultHasher, Hash, Hasher};
-use std::io::prelude::*;
-use std::io::{self, stdin, BufRead, BufReader, Read, Write};
+use std::io::{self, BufRead, BufReader, Read, Write, stdin};
 use std::os::unix::io::AsRawFd;
 
 fn main() {
     loop {
         let mut response = String::new();
-        get_input(
+        match get_input(
             r#"Welcome to Cruz Password Manager
 press 1 to create an account
 press 2 to login to an account
 press q to quit
 "#,
             &mut response,
-        );
+        ) {
+            Ok(_) => (),
+            Err(e) => println!("Could not get input due to: {}", e),
+        }
 
         let mut key = String::new();
         let mut user_name = String::new();
         if response == "1" {
-            signup(&mut key).expect("should be handled in the code");
+            match signup(&mut key) {
+                Err(e) => println!("Couldn't signup: {}", e),
+                _ => (),
+            }
         } else if response == "2" {
             let login_sesh = login(&mut key, &mut user_name);
-            if login_sesh.is_ok() {
-                user_session(&key, &user_name);
-            } else {
-                println!("{}", login_sesh.err().unwrap())
-            };
+            match login_sesh {
+                Ok(_) => user_session(&key, &user_name),
+                Err(e) => println!("{}", e),
+            }
         } else if response.to_lowercase() == "q" {
             println!("Thanks for using the Password Manager");
             break;
@@ -37,13 +42,13 @@ press q to quit
     }
 }
 
-fn signup(key: &mut String) -> Result<String, ()> {
+fn signup(key: &mut String) -> Result<String, Box<dyn Error>> {
     let mut username = String::new();
-    get_input("Input your username", &mut username);
+    get_input("Input your username", &mut username)?;
 
     let mut password = String::new();
     let mut password_hasher = DefaultHasher::new();
-    get_masked_input("Please input a password", &mut password);
+    get_masked_input("Please input a password", &mut password)?;
 
     get_symmetric_key(key, &password);
     password.hash(&mut password_hasher);
@@ -57,21 +62,20 @@ fn signup(key: &mut String) -> Result<String, ()> {
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(database)
-        .unwrap();
-    file.write_all(user.as_bytes()).unwrap();
+        .open(database)?;
+    file.write_all(user.as_bytes())?;
     Ok(key.to_string())
 }
 
 fn login<'a>(
     key: &'a mut String,
     user_name: &mut String,
-) -> Result<&'a mut String, Box<dyn std::error::Error>> {
+) -> Result<&'a mut String, Box<dyn Error>> {
     let mut username = String::new();
     let mut password = String::new();
 
-    get_input("Input your username", &mut username);
-    get_masked_input("Input your password", &mut password);
+    get_input("Input your username", &mut username)?;
+    get_masked_input("Input your password", &mut password)?;
 
     get_symmetric_key(key, &password);
     let mut password_hasher = DefaultHasher::new();
@@ -110,15 +114,27 @@ fn hash_key(key: &mut String, password: &String) {
 fn user_session(key: &String, user: &String) {
     loop {
         let mut response = String::new();
-        get_input(
+        match get_input(
             "press 1 to add a password\npress 2 to retrieve a password\npress b to go back",
             &mut response,
-        );
+        ) {
+            Ok(_) => (),
+            Err(e) => {
+                println!("error getting input: {}", e);
+                continue;
+            }
+        }
 
         if response == "1" {
-            add_password(key, user);
+            match add_password(key, user) {
+                Ok(_) => (),
+                Err(e) => println!("Could not add password because of: {}", e),
+            };
         } else if response == "2" {
-            retrieve_password(key, user).unwrap();
+            match retrieve_password(key, user) {
+                Ok(_) => (),
+                Err(e) => println!("Could not retrieve password because of: {}", e),
+            }
         } else if response == "b" {
             break;
         } else {
@@ -127,16 +143,16 @@ fn user_session(key: &String, user: &String) {
     }
 }
 
-fn add_password(key: &String, user: &String) {
+fn add_password(key: &String, user: &String) -> Result<(), Box<dyn Error>> {
     let mut account = String::new();
     get_input(
         "\nWhat is the name of the account associated with the password",
         &mut account,
-    );
+    )?;
 
     let mut password = String::new();
     println!("Type in the passowrd");
-    get_masked_input("Type in the password", &mut password);
+    get_masked_input("Type in the password", &mut password)?;
 
     let password = encrypt_password(key, &password);
     let response = account.clone() + " " + password.as_str() + "\n";
@@ -146,12 +162,12 @@ fn add_password(key: &String, user: &String) {
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(database)
-        .unwrap();
-    file.write_all(response.as_bytes()).unwrap();
+        .open(database)?;
+    file.write_all(response.as_bytes())?;
 
     println!("encryped password is {password}");
     println!("password added to {account} account");
+    Ok(())
 }
 
 fn retrieve_password(key: &String, user: &String) -> Result<(), io::Error> {
@@ -159,15 +175,21 @@ fn retrieve_password(key: &String, user: &String) -> Result<(), io::Error> {
     get_input(
         "\nWhat is the name of the account associated with the password",
         &mut account,
-    );
+    )?;
 
     let database = user.clone() + "_database.db";
     let database = database.as_str();
 
-    let file = File::open(database)?;
+    let file = match File::open(database) {
+        Ok(x) => x,
+        Err(e) => {
+            println!("unable to open file: {}", e);
+            return Err(e);
+        }
+    };
     let reader = BufReader::new(file);
     for line in reader.lines() {
-        let line = line.unwrap();
+        let line = line?;
         println!("{line}");
         let u: Vec<_> = line.split(" ").collect();
         if account == u[0] {
@@ -232,19 +254,19 @@ fn set_raw_mode(enable: bool) {
     }
 }
 
-fn get_masked_input(query: &str, input: &mut String) {
+fn get_masked_input(query: &str, input: &mut String) -> io::Result<()> {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
     *input = String::new();
 
     println!("{query}");
-    stdout.flush().unwrap();
+    stdout.flush()?;
 
     set_raw_mode(true);
 
     let mut buf = [0u8; 1];
     loop {
-        stdin.read_exact(&mut buf).unwrap();
+        stdin.read_exact(&mut buf)?;
         let ch = buf[0] as char;
 
         if ch == '\n' || ch == '\r' {
@@ -253,21 +275,23 @@ fn get_masked_input(query: &str, input: &mut String) {
             if !input.is_empty() {
                 input.pop();
                 print!("\x08 \x20 \x08");
-                stdout.flush().unwrap();
+                stdout.flush()?;
             }
         } else {
             input.push(ch);
             print!("*");
-            stdout.flush().unwrap();
+            stdout.flush()?;
         }
     }
 
     set_raw_mode(false);
     println!("\nthe input typed is {input}");
+    Ok(())
 }
 
-fn get_input(query: &str, output: &mut String) {
+fn get_input(query: &str, output: &mut String) -> io::Result<()> {
     println!("{query}");
-    stdin().read_line(output).unwrap();
+    stdin().read_line(output)?;
     *output = output.trim().to_string();
+    Ok(())
 }
